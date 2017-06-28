ff57f53dcf60fc75176bc5e059b0a8f230c7da51
Sarvesh kumar <sarvesh.carlton@gmail.com>
2017-06-28 13:23:01 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     // url: 'mongodb://localhost:27017/testDb'
-    url: 'mongodb://sarveshome:Bcamcaphd@761@ds159237.mlab.com:59237/awsnode'
+    url: 'mongodb://sarveshome:Bcamcaphd@761@ds135812.mlab.com:35812/testmongodb' 
 }
